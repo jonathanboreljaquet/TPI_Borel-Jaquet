662e80d4b052816c8a6c9c644a15befe1cb5b392
jonathanboreljaquet <jonathan.brljq@eduge.ch>
2019-05-07 16:46:13 +0200

    Fin de journée 07/05/2019
    Début de la création du template Web Responsiv

---
+++ TPI/www/template/templateWeb.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Template WEB Responsive</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+    <nav class="topnav">
+        
+        <a class="navbar-brand" href="#">Navbar</a>
+
+        <a class="navbar-brand" href="#">Navbar</a>
+
+    </nav>
+</body>
+
+</html>
